4cb76daa9babaa96d1de2ebbbdc0e8abd49d1523
sonic-slogup <phg0644@slogup.com>
2022-09-25 16:16:58 +0900

    done : set cors

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,12 @@ const openapiSpec = swaggerJsdoc(options);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
+/**
+ * cors Setting
+ */
+var cors = require("cors");
+app.use(cors());
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
